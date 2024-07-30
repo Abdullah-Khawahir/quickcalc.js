16d83fad6df78a9620e34c4494f19a85b56ad1cb
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-07-30 22:58:01 +0300

    convert checks in the same unit category

--- src/convert.js
+++ src/convert.js
@@ -1,4 +1,4 @@
-import { units, areSameCatagory, breakCompoundUnit, isTemprtureUnit, temprtureUnitsToKelvin, getConvertingFunction, areAreaUnits } from './units.js';
+import { units, areSameCatagory, getUnitCatagory, breakCompoundUnit, isTemprtureUnit, temprtureUnitsToKelvin, getConvertingFunction, areAreaUnits } from './units.js';
 /**
  * Converts a value from one unit to another.
  * @param {number} value - The value to convert.
@@ -11,9 +11,11 @@ export function convert(value, fromUnit, toUnit) {
 	const fromCompoundUnit = breakCompoundUnit(fromUnit)
 	const toCompoundUnit = breakCompoundUnit(toUnit)
 	if (fromCompoundUnit !== undefined && toCompoundUnit !== undefined) {
+		const unit1Catagory = getUnitCatagory(fromCompoundUnit.unit1)
+		const unit2Catagory = getUnitCatagory(fromCompoundUnit.unit2)
 
-		const baseValue = value * (units[fromCompoundUnit.unit1] / units[fromCompoundUnit.unit2])
-		const converted = baseValue / (units[toCompoundUnit.unit1] / units[toCompoundUnit.unit2])
+		const baseValue = value * (unit1Catagory[fromCompoundUnit.unit1] / unit2Catagory[fromCompoundUnit.unit2])
+		const converted = baseValue / (unit1Catagory[toCompoundUnit.unit1] / unit2Catagory[toCompoundUnit.unit2])
 		return converted
 	}
 
@@ -36,8 +38,9 @@ export function convert(value, fromUnit, toUnit) {
 	if (!areSameCatagory(fromUnit, toUnit)) {
 		throw new Error(`the units are not the same category: ${fromUnit} , ${toUnit}`)
 	}
-	const baseValue = value * units[fromUnit];
-	const convertedValue = baseValue / units[toUnit];
+	const unitCatagory = getUnitCatagory(fromUnit)
+	const baseValue = value * unitCatagory[fromUnit];
+	const convertedValue = baseValue / unitCatagory[toUnit];
 
 	return convertedValue
 }
